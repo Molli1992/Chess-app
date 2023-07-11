1fed4a0b7c3f06868ca9fe19de7e06297e817fae
Totoblak90 <tobias.blaksley@hotmail.com>
2023-07-11 18:57:20 -0300

    guardando

--- src/components/learn/learn.jsx
+++ src/components/learn/learn.jsx
@@ -8,7 +8,9 @@ import YouTube from 'react-youtube';
 
 function Learn() {
 
-    const videoId = 'e57NqRHE6AY';
+    const video1 = 'e57NqRHE6AY';
+    const video2 = "rv9O8I1kNnc";
+    const video3 = "soeEeadcOKw";
 
 
     return (
@@ -35,34 +37,24 @@ function Learn() {
 
                     <div className="video">
                         <h3>Learn to play from scratch</h3>
-                        <YouTube videoId={videoId} />
+                        <YouTube videoId={video1} />
                     </div>
 
                     <div className="video">
                         <h3>Learn to play from scratch</h3>
-                        <YouTube videoId={videoId} />
+                        <YouTube videoId={video2} />
                     </div>
 
                     <div className="video">
                         <h3>Learn to play from scratch</h3>
-                        <YouTube videoId={videoId} />
+                        <YouTube videoId={video3} />
                     </div>
 
-                    <div className="video">
-                        <h3>Learn to play from scratch</h3>
-                        <YouTube videoId={videoId} />
-                    </div>
+                </div>
 
-                    <div className="video">
-                        <h3>Learn to play from scratch</h3>
-                        <YouTube videoId={videoId} />
-                    </div>
-
-                    <div className="video">
-                        <h3>Learn to play from scratch</h3>
-                        <YouTube videoId={videoId} />
-                    </div>
-
+                <div className="button-container-learn">
+                    <button className="button-learn">Prev</button>
+                    <button className="button-learn">Next</button>
                 </div>
 
             </div>
